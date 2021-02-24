8fb24ffad938d1ffd6ff98b87004c80024c4dd69
yasmeenkhalili <jasmina201132@yahoo.com>
2021-02-24 11:36:50 +0200

    stater-code

---
+++ javacode.js
@@ -0,0 +1,13 @@
+  
+var today= new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+greeting= 'Good evening!';
+}else if (hourNow > 12) {
+greeting = ' Good afternoon!';
+}else if (hourNow > 0) {
+greeting = 'Good morni ng!';
+}else
+greeting = 'Welcome! ' ;
+document .write( ' <h3>' +greeting + ' </ h3> ');
